fb14681df1918528402e5dd364a6c59f1cc924e2
errajesh29 <32628652+errajesh29@users.noreply.github.com>
2019-09-14 16:30:34 +0530

    Update index.js
    Removed kkye

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 
 const SERVICE_URL = 'https://api.yelp.com/v3';
-const API_KEY = 'tq0_XE1VmGox4Bew_Yu92xVBHB7ZgfdSgnryUMhVyA9GFc7_YbF098d7nHaJ3tTMGAuPgmveLy_7UkPmiSSmBe0kpFC_33VpVhIDv2kSRJwWsq-OAqLYkFpE1eZhXXYx';
+const API_KEY = 'key'
 
 module.exports = { SERVICE_URL, API_KEY };
